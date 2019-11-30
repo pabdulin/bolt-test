33a0e6ffed121556e4da8dae7554a77ede78a57e
Petr Abdulin <pabdulin@gmail.com>
2019-11-30 20:52:22 +0700

    Initial commit (live coding original result).

---
+++ Demo.Lib/Problem.cs
@@ -0,0 +1,97 @@
+﻿using System;
+
+namespace Demo.Lib
+{
+    /*
+Write an algorithm which reverses the letters in words, like: 
+Input: I love Taxify 
+Output:I evol yfixaT
+Input is character array (not string) and output should also be character array. Function needs to work in-place, modify the input array itself. Don't use any extra arrays, string objects or language provider libraries. Use of temporary variables is allowed.
+Example in Java:
+Input: char[] input = ['I', ' ', 'l','o','v','e',' ','T','a','x','i','f','y'];
+public static char[] reverseWords(char [] input) {
+	// write your code here
+	return input;
+}
+Output: ['I', ' ', 'e','v','o','l',' ','y','f','i','x','a','T'];
+
+Example is in Java, but choice of programming language is free.Please write the code in the web browser, don't test or run it in your IDE.
+Extra: you will earn extra points if you write unit tests for this function.
+
+Live codiong result: https://www.interviewzen.com/interview/4FPFdr3
+     */
+    public class Problem
+    {
+        public static char[] reverseWords(char[] input)
+        {
+            if (input == null)
+            {
+                throw new ArgumentNullException(nameof(input));
+            }
+
+            var wordStart = 0;
+            while (true)
+            {
+                if (wordStart >= input.Length)
+                {
+                    // end of char array
+                    break;
+                }
+
+                // skip to word start
+                for (var i = wordStart; i < input.Length; i += 1)
+                {
+                    var testChar = input[i];
+                    wordStart = i;
+                    if (!isWhitespace(testChar))
+                    {
+                        break;
+                    }
+                }
+
+                // char is not actually a symbol, it is at the end of the input then
+                // == end of char array with whitespace
+                if (isWhitespace(input[wordStart]))
+                {
+                    break;
+                }
+
+                // seek for wordEnd
+                var wordEnd = wordStart;
+                for (var i = wordEnd + 1; i < input.Length; i++)
+                {
+                    var testChar = input[i];
+                    if (isWhitespace(testChar))
+                    {
+                        break;
+                    }
+                    wordEnd = i;
+                }
+
+                // indices found, now reverse
+                reverseChars(wordStart, wordEnd, input);
+
+                // advance to next possible word
+                wordStart = wordEnd + 1;
+            }
+        }
+
+        public static bool isWhitespace(char symbol)
+        {
+            return symbol == ' ';
+        }
+
+        public static void reverseChars(int start, int end, char[] input)
+        {
+            while ((end - start) > 0)
+            {
+                var tempStart = input[start];
+                input[start] = input[end];
+                input[end] = tempStart;
+                end -= 1;
+                start += 1;
+            }
+        }
+
+    }
+}
---
+++ Demo.Tests/ProblemTest.cs
@@ -0,0 +1,70 @@
+using Demo.Lib;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace Demo.Tests
+{
+    /*
+Write an algorithm which reverses the letters in words, like: 
+Input: I love Taxify 
+Output:I evol yfixaT
+Input is character array (not string) and output should also be character array. Function needs to work in-place, modify the input array itself. Don't use any extra arrays, string objects or language provider libraries. Use of temporary variables is allowed.
+Example in Java:
+Input: char[] input = ['I', ' ', 'l','o','v','e',' ','T','a','x','i','f','y'];
+public static char[] reverseWords(char [] input) {
+	// write your code here
+	return input;
+}
+Output: ['I', ' ', 'e','v','o','l',' ','y','f','i','x','a','T'];
+
+Example is in Java, but choice of programming language is free.Please write the code in the web browser, don't test or run it in your IDE.
+Extra: you will earn extra points if you write unit tests for this function.
+     */
+    [TestClass]
+    public class ProblemTest
+    {
+        // tests
+        [TestMethod]
+        public void testReverseWords()
+        {
+            var testSamples = new Dictionary<char[], char[]>()
+            {
+                {new char[] {}, new char[] {}}, // empty array
+                {new char[] {' '}, new char[] {' '}}, // no words
+                {new char[] {'i'}, new char[] {'i'}}, // odd length word
+                {new char[] {'i', '1'}, new char[] {'1', 'i'}}, // even length word
+                {new char[] {' ', 'i', '1', ' '}, new char[] {' ', '1', 'i', ' '}}, // leading and trailing spaces
+                {new char[] {'I', ' ', 'l','o','v','e',' ','T','a','x','i','f','y'}, new char[] {'I', ' ', 'e','v','o','l',' ','y','f','i','x','a','T'}}, // demo sample
+            };
+
+            foreach (var sample in testSamples)
+            {
+                runTestSample(sample.Key, sample.Value);
+            }
+        }
+
+        public static void runTestSample(char[] test, char[] expected)
+        {
+            var result = Problem.reverseWords(test);
+            Assert.IsTrue(areEqual(result, expected));
+        }
+
+        public static bool areEqual(char[] a1, char[] a2)
+        {
+            if (a1 == null || a2 == null || (a1.Length != a2.Length))
+            {
+                return false;
+            }
+
+            for (int i = 0; i < a1.Length; i += 1)
+            {
+                if (a1[i] != a2[i])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
